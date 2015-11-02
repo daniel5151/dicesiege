ba12afcbd96bf138a7cd4aff78d35ff3617edcfe
Daniel Prilik <danielprilik@gmail.com>
2015-11-02 12:21:18 -0500

    Where has CreateJS been all my life!

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function render() {
+    //
+}
+
+function logic() {
+    //
+}
+
+function init() {
+    var stage = new createjs.Stage("c");
+    stage.canvas.width = window.innerWidth;
+    stage.canvas.height = window.innerHeight;
+
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+
+    createjs.Tween.get(circle, { loop: false })
+        .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+        .to({ alpha: 0, y: 225 }, 100)
+        .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+        .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", stage);
+}
+
+window.onload = init;
