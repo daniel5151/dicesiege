5af8d66736365dabebe211df686e4352fb57be8c
Daniel Prilik <danielprilik@gmail.com>
2015-11-03 16:12:15 -0500

    MmmmMmmm, that's one hell of a grid

--- js/main.js
+++ js/main.js
@@ -1,30 +1,85 @@
-function render() {
-    //
+var stage;
+
+var board_prefs = {
+    size:{
+        h:10,
+        w:10
+    },
+    offset:{
+        x:60,
+        y:60
+    },
+    hexdims:{
+        s:40,
+        h:'computed',
+        w:'computed'
+    }
 }
 
-function logic() {
-    //
+function NewHex (i,j,hexdims,offset) {
+    var hex = new createjs.Shape;
+
+    var hexY = offset.y + i * hexdims.h;
+    if (i % 2 == 0) var hexX = offset.x + j * hexdims.w;
+    else            var hexX = offset.x + j * hexdims.w + 1/2* hexdims.w;
+
+    hex.graphics
+        .beginStroke("#aaa")
+        .beginLinearGradientFill(
+            ["#eee","#fafafa"],
+            [0, 1], 0, hexY-20, 0, hexY+30
+        )
+        .drawPolyStar(hexX,hexY,hexdims.s,6,0,30)
+        .endStroke()
+        .endFill();
+
+    var text = new createjs.Text(i + ", " + j, "16px Arial", "black");
+        text.x = hexX-15;
+        text.y = hexY+5;
+        text.textBaseline = "alphabetic";
+
+    var container = new createjs.Container();
+    container.addChild(hex);
+    container.addChild(text)
+    return container;
 }
+
+function Board(prefs) {
+    this.size = prefs.size;
+    this.offset = prefs.offset;
+    this.hexdims = {
+        s: prefs.hexdims.s,
+        w: Math.sqrt(3)/2 * 2 * prefs.hexdims.s,
+        h:            3/4 * 2 * prefs.hexdims.s
+    };
+
+    this.init = function () {
+        this.map = [];
+        for (var i = 0; i < this.size.h; i++) {
+            this.map.push([]);
+            for (var j = 0; j < this.size.w; j++) {
+                this.map[i][j] = NewHex(i, j, this.hexdims, this.offset);
+                stage.addChild(this.map[i][j]);
+            }       
+        }
+    }
+}
+
+var board;
 
 function init() {
-    var stage = new createjs.Stage("c");
+    //Create stage object - our root level container
+    stage = new createjs.Stage("c");
     stage.canvas.width = window.innerWidth;
     stage.canvas.height = window.innerHeight;
 
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
+    // stage.enableMouseOver(10);
+    
+    // Call the function to craete the hex grid
+    board = new Board(board_prefs);
+    board.init();
 
-    createjs.Tween.get(circle, { loop: false })
-        .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ alpha: 0, y: 225 }, 100)
-        .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
-
-    createjs.Ticker.setFPS(60);
+    // updaet canvas 24/7, b/c fuck efficiency
     createjs.Ticker.addEventListener("tick", stage);
 }
 
