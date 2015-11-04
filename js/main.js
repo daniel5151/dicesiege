872426c101adfd8a0140f69c1346bdc075c18934
Daniel Prilik <danielprilik@gmail.com>
2015-11-04 00:01:03 -0500

    getting the board to work just how I like it

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var debug = true;
 
 // Global vars
 var stage;
+var board;
 
 // Board preferences
 var board_prefs = {
@@ -14,8 +15,8 @@ var board_prefs = {
 
 function Hex (row,col,hexsize) {
     // calculate hex dims based on size
-    hexW = Math.sqrt(3)/2 * 2 * hexsize;
-    hexH =          (3)/4 * 2 * hexsize;
+    var hexW = Math.sqrt(3)/2 * 2 * hexsize;
+    var hexH =          (3)/4 * 2 * hexsize;
 
     // create createJS shape
     this.shape = new createjs.Shape;
@@ -39,63 +40,115 @@ function Hex (row,col,hexsize) {
         )
         .endStroke()
         .endFill();
+}
 
-    // grid debug
-    if (debug) {
-        var text = new createjs.Text(row + ", " + col, hexsize/2 + "px Arial", "black");
-            text.x = board.mapContainer.x + this.shape.x - 15;  // yeah, i'm referencing board. dill with it
-            text.y = board.mapContainer.y + this.shape.y + 5;
-            text.textBaseline = "alphabetic";
+var Debug = {
+    GridText:function(row, col, hexsize) {
+        // calculate hex dims based on size
+        var hexW = Math.sqrt(3)/2 * 2 * hexsize;
+        var hexH =          (3)/4 * 2 * hexsize;
 
-        stage.addChild(text)
+        // set co-ordinates of hex on canvas
+                          var y = row * hexH;
+        if (row % 2 == 0) var x = col * hexW;
+        else              var x = col * hexW + 1/2* hexW;
+
+        this.text = new createjs.Text(row + ", " + col, hexsize/2 + "px Arial", "black");
+
+        this.text.x            = board.offset.x + x - hexsize/2;  // yeah, i'm referencing board. dill with it
+        this.text.y            = board.offset.y + y + hexsize/8;
+        this.text.textBaseline = "alphabetic";
     }
 }
 
 function Board(prefs) {
-    this.dims = prefs.dims;
-
-    this.mapContainer = new createjs.Container();
-
-    // scale grid to screen
-    prefs.hexsize = prefs.hexsize || Math.floor( Math.min(stage.canvas.width, stage.canvas.height) / prefs.dims.w) / 2;
-
-    // center the grid on the board
-    this.mapContainer.x = (stage.canvas.width  - (prefs.dims.w - 0.5) * Math.sqrt(3)/2 * 2 * prefs.hexsize) / 2;
-    this.mapContainer.y = (stage.canvas.height - (prefs.dims.h - 0.5) *          (3)/4 * 2 * prefs.hexsize) / 2;
+    this.dims    = prefs.dims;
+    this.hexsize = prefs.hexsize;
+    this.offset  = prefs.offset;
+    
+    this.centerOffset = function () {
+        this.offset = {
+            x:(stage.canvas.width  - (this.dims.w - 0.5) * Math.sqrt(3)/2 * 2 * this.hexsize) / 2,
+            y:(stage.canvas.height - (this.dims.h - 0.5) *          (3)/4 * 2 * this.hexsize) / 2
+        };
+        this.mapContainer.x = this.offset.x;
+        this.mapContainer.y = this.offset.y;
+    }
 
     this.init = function () {
+        // scale grid to screen
+        this.hexsize = this.hexsize || 
+            Math.floor( Math.min(stage.canvas.width, stage.canvas.height) / Math.min(this.dims.w, this.dims.h) ) / 2;
+
+        this.mapContainer = new createjs.Container();
+
+        // center the grid on the board
+        this.centerOffset();
+
+        // draw the grid
         stage.addChild(this.mapContainer);
 
         this.mapObjects = [];
         for (var row = 0; row < this.dims.h; row++) {
             this.mapObjects.push([]);
             for (var col = 0; col < this.dims.w; col++) {
-                this.mapObjects[row][col] = new Hex(row, col, prefs.hexsize);
-
+                this.mapObjects[row][col] = new Hex(row, col, this.hexsize);
                 this.mapContainer.addChild(this.mapObjects[row][col].shape);
             }       
         }
 
-        stage.update()
+        //----------------- DEBUG
+            if (debug) {
+                this.debugContainer = new createjs.Container();
+                stage.addChild(this.debugContainer)
+
+                this.debugObjects = [];
+                for (var row = 0; row < this.dims.h; row++) {
+                    this.debugObjects.push([]);
+                    for (var col = 0; col < this.dims.w; col++) {
+                        this.debugObjects[row][col] = new Debug.GridText(row, col, this.hexsize);
+
+                        this.debugContainer.addChild(this.debugObjects[row][col].text);
+                    }       
+                }
+            }
+        //------------- END DEBUG
+
+        stage.update();
+    }
+
+    this.resize = function (newsize){
+        // remove all children so we can repopulate the scene.
+        stage.removeAllChildren();
+
+        // set new hex size
+        this.hexsize = 
+            (newsize == "towindow")
+                ? Math.floor( Math.min(stage.canvas.width, stage.canvas.height) / Math.min(board.dims.w, board.dims.h) ) / 2
+                : newsize
+        ;
+
+        // reinit board
+        this.init();
     }
 }
-
-var board;
 
 function init() {
     //Create stage object - our root level container
     stage = new createjs.Stage("c");
     stage.canvas.width = window.innerWidth;
     stage.canvas.height = window.innerHeight;
-
-    // stage.enableMouseOver(10);
     
     // Call the function to craete the hex grid
     board = new Board(board_prefs);
     board.init();
 
-    // updaet canvas 24/7, b/c fuck efficiency
-    // createjs.Ticker.addEventListener("tick", stage);
+    // resize means we have to rescale everything
+    window.addEventListener('resize', function(e){
+        stage.canvas.width = window.innerWidth;
+        stage.canvas.height = window.innerHeight;      
+        board.resize("towindow");
+    }, false);
 }
 
 window.onload = init;
